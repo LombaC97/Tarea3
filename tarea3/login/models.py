b6d51166b9a8e87853dab4c9aafef78219f19136
Mafe <mafermach@gmail.com>
2020-02-07 17:14:33 -0400

    mejoreas22

--- tarea3/login/models.py
+++ tarea3/login/models.py
@@ -5,10 +5,9 @@ from .validators import validar_clave, validar_correo
 # Create your models here.
 
 class seguridad(models.Model):
-
+	# Da el correo del usuario mas no la contrasena
 	correo = models.CharField(max_length=200, validators=[validar_correo])
 	clave = models.CharField(max_length=16, validators=[validar_clave])
-
 
 	def __str__(self):
 		return self.correo + ': ' + self.clave[::-1]
--- tarea3/tarea3/views.py
+++ tarea3/tarea3/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 
+# Para que el servidor comience en login
 def redirect_view(request):
     response = redirect('login/')
     return response
