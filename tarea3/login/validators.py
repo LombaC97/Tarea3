4fc44253681245af1a24bd8fd26a657b589bfdb9
Mafe <mafermach@gmail.com>
2020-02-07 17:12:09 -0400

    mejoras

--- tarea3/login/validators.py
+++ tarea3/login/validators.py
@@ -1,9 +1,27 @@
+import re
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-def validar_correo(value):
-	pass
+# Datos permitidos en el correo
+regexCorreo = '^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$'
 
+# Se valida que el correo sea valido
+def validar_correo(value):
+	acepto = re.search(regexCorreo, value)
+	if(not acepto):
+		raise ValidationError(_('%(value)s correo invalido'), params={'value': value},)
+
+# Verifia si la contrasena es correcto
 def validar_clave(value):
-	pass
+	if(not len(value) <= 8 and len(value) >= 16 ):
+		raise ValidationError(_('%(value)s contrasena invalida'), params={'value': value},)
+
+# Verifica si las contrasenas son iguales
+def validar_iguales(value1, value2):
+	igual = (value1 == value2)
+	if(not igual):
+		raise ValidationError(_('%(value2)s no coinciden las contrasenas'), params={'value1': value1, 'value2': value2},)
+
+
+
--- tarea3/login/views.py
+++ tarea3/login/views.py
@@ -5,9 +5,7 @@ from .models import seguridad
 from .funciones import registrarUsuario, ingresarUsuario
 # Create your views here.
 
-
 def index(request):
-	
 	template= loader.get_template('login/login.html')
 	contexto={}
 	return HttpResponse(template.render(contexto, request))
@@ -15,18 +13,16 @@ def index(request):
 def home(request, correo):
 	template= loader.get_template('login/home.html')
 	contexto={
-
 	'email': correo,
 	}
 	return HttpResponse(template.render(contexto, request))
 
+# Atentifica los correos
 def autentificar(request):
 	try:
 		correo = request.GET['correo']
 		contrasena = request.GET['contrasena']
 		existe, data = ingresarUsuario(correo, contrasena)
-		# validaciones como que ajuro entre al correo
-		# Busca al usurio enn la bd
 	# Cuando no se introdujo bien el correo lanza error
 	except KeyError:
 		texto = { 'error_message': "Vuelva a intentar"}
@@ -42,20 +38,18 @@ def autentificar(request):
 			texto = { 'error_message': "el usuario no es valido"}
 			return render(request, 'login/login.html')
 
-
+# Registro para los usuarios
 def register(request):
 	template = loader.get_template('login/register.html')
 	contexto={}
-
 	return HttpResponse(template.render(contexto,request))
 
+# Registro de usuario
 def register_usuario(request):
 	try:
 		correo = request.POST['correo']
 		contrasena = request.POST['contrasena']
 		contrasena2= request.POST['contrasena2']
-
-		#Validaciones
 
 	except KeyError:
 		texto = { 'error_message': "Vuelva a intentar"}
